f013179b3e9f9e2854c86aa8f7b7d389acd4abb7
TotallyNotMrH <thomas.hecht@bsz-wiesau.de>
2023-08-17 11:10:13 +0200

    Tastersteuerung
    Dieser Code steuert eine LED mithilfe eines Tasters

---
+++ HelloWorld/SourceCodeLedMitTaster.ino
@@ -0,0 +1,19 @@
+ #define PIN_LED 13
+#define PIN_REED 15
+
+void setup() {
+  
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_REED, INPUT_PULLUP);
+}
+
+void loop() {
+
+  int rState = digitalRead(PIN_REED);
+  int myState = HIGH;
+  if (rState) {
+   myState = LOW;
+  }
+  digitalWrite(PIN_LED, myState);
+  
+}
